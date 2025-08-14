4d941dc7f4bec1c2256d775e113f632c09c7b8ad
Dylan <dylan.daev@gmail.com>
2025-08-13 19:07:50 -0600

    feat: add about section

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -20,6 +20,13 @@ export const ui = {
     "hero.button.projects": "Ver proyectos",
     "hero.button.contact": "Contacto",
     "hero.console.specialization": "Desarrollo Backend",
+    "about.title": "Sobre mí",
+    "about.description.1":
+      "Soy estudiante de último año de Ingeniería en Ciencias y Sistemas, enfocado en el desarrollo backend con Java. Tengo experiencia en la construcción de APIs con Spring Boot y Postgres, pero también he trabajado con tecnologías como Python y Express.js.",
+    "about.description.2":
+      "Además de mi experiencia en el desarrollo de backend, también he trabajado con TypeScript con Tailwind CSS y en la integración continua con Docker y GitHub Actions para proyectos académicos.",
+    "about.description.3":
+      "Mi objetivo es seguir creciendo como desarrollador, aprendiendo nuevas tecnologías y enfrentando desafíos en el mundo del desarrollo de software.",
     "repoCard.view": "Ver detalles",
     "repoCard.source": (...args: any[]) =>
       args[0] ? `Ver código del ${args[0]}` : `Ver código`,
@@ -38,6 +45,13 @@ export const ui = {
     "hero.button.projects": "View Projects",
     "hero.button.contact": "Contact",
     "hero.console.specialization": "Backend Development",
+    "about.title": "About Me",
+    "about.description.1":
+      "I'm a student of the last year of Engineering in Sciences and Systems, focused on backend development with Java. I have experience in building APIs with Spring Boot and Postgres, but I also worked with technologies like Python and Express.js.",
+    "about.description.2":
+      "In addition to my experience in backend development, I have also worked with TypeScript with Tailwind CSS and in continuous integration with Docker and GitHub Actions for academic projects.",
+    "about.description.3":
+      "My goal is to continue growing as a developer, learning new technologies, and facing challenges in the world of software development.",
     "repoCard.view": "View Details",
     "repoCard.source": (...args: any[]) =>
       args[0] ? `View ${args[0]} code` : `View code`,
